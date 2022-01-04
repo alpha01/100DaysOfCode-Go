4d077bf9fa4e01c30a1483933d141f7de8a74632
Alpha01 <root@rubyninja.org>
2022-01-03 21:56:58 -0800

    Day 3

---
+++ week1/conditions/src/conditions.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	if true {
+		fmt.Printf("This is true!\n")
+	}
+
+	if !false {
+		fmt.Printf("Double negative!!\n")
+	}
+}
---
+++ week1/string_formating/src/testing_string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	p := "Alpha01"
+	t := fmt.Sprintf("The value of p is %v", p)
+
+	fmt.Println(t)
+
+}
